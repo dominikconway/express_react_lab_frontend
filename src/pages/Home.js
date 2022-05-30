ed1bd2faf5c432e715b8d9e4e5e2a8b2dd313659
Dominik Conway <dominik.conway@gmail.com>
2022-05-30 11:15:27 -0700

    adds content to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,9 @@
 export default function Home(props) {
-    return <h1>Home</h1>
+    return (
+    <div>
+        <h1>Home</h1>
+        <h3>Add some content to the home page</h3>
+    </div>
+    )
 }
 
